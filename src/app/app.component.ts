1764e677774e30f2e4d36adcea1fb4ed40c90020
Brooke Clonts <brooke.clonts@ariix.com>
2017-01-13 15:33:51 -0700

    banner move

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,34 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-    loader: boolean = false;
+    loader: boolean = true;
+    fadeOut: boolean = false;
 
     constructor() {}
 
+    moveLoader() {
+        let banner = document.getElementById('homeTat');
+        let loader = document.getElementById('loaderTat');
+        let bannerOffset = banner.getBoundingClientRect().top;
+        console.log(banner.getBoundingClientRect());
+        console.log(bannerOffset);
+        loader.setAttribute('style', `margin-top: ${bannerOffset}px; top: 0`);
+    }
+
     showLoader() {
-        this.loader = true;
         setTimeout(() => {
-            this.loader = false;
-        }, 5000);
+            this.moveLoader();
+            this.fadeOutLoader();
+        }, 2500);
+    }
+
+    fadeOutLoader() {
+        setTimeout(() => {
+            this.fadeOut = true;
+            setTimeout(() => {
+                this.loader = false;
+            }, 500);
+        }, 500);
     }
 
     ngOnInit() {
