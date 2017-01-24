8361c52b4f68fe05fdbee09cbd21ea4478c8f458
Brooke Clonts <brooke.clonts@ariix.com>
2017-01-23 18:45:43 -0700

    added mailchimp api

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-var request = require('request');
 
 // Get our API routes
 const api = require('./server/routes/api');
@@ -19,29 +18,6 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // Set our api routes
 app.use('/api', api);
-
-app.get('/mailchimp',function(res,req){
-    let auth = 'Basic ' + btoa('user:edb30ff6e207bb68b6c25fccca8e723b-us14');
-    let url = 'https://us14.api.mailchimp.com/3.0/lists/b762b0fa4f/members';
-
-    request(
-      {
-          url : url,
-          headers : {
-              "Authorization" : auth,
-              'Content-Type' : 'application/json'
-          }
-      },
-      function (error, response, body) {
-          if (error) {
-            console.log(error);
-            return error;
-          }
-          console.log(response);
-          return response;
-      }
-  );
-});
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
 });
+
+router.post('/mailchimp', function(req, res) {
+    var username = 'user'
+    var password = 'edb30ff6e207bb68b6c25fccca8e723b-us14'
+    var options = {
+        uri: 'https://us14.api.mailchimp.com/3.0/lists/b762b0fa4f/members',
+        method: 'POST',
+        json: {
+            "email_address": req.body.email,
+            "status": "subscribed",
+            "merge_fields": {
+                "FNAME": req.body.fName,
+                "LNAME": req.body.lName
+            }
+        },
+        headers: {
+            'Authorization': 'brookeclonts edb30ff6e207bb68b6c25fccca8e723b-us14',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    request(options, function (err, result, body) {
+      if (err) {
+        res.send(err);
+        return
+      }
+      res.send(result);
+    })
+});
+
 
 module.exports = router;
