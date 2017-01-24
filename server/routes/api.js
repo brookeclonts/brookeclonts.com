25ba67fd4aeb84c3d100dec22f371cae237474c3
Brooke Clonts <brooke.clonts@ariix.com>
2017-01-24 14:38:23 -0700

    added mailchimp env variables

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,9 +9,10 @@ router.get('/', (req, res) => {
 
 router.post('/mailchimp', function(req, res) {
     var username = 'user'
-    var password = 'c8a050996a47d859d87a198db8cb3cba-us14'
+    var password = process.env.MAILCHIMP_KEY;
+    var datacenter = password.split('-')[1];
     var options = {
-        uri: 'https://us14.api.mailchimp.com/3.0/lists/b762b0fa4f/members',
+        uri: `https://${datacenter}.api.mailchimp.com/3.0/lists/b762b0fa4f/members`,
         method: 'POST',
         json: {
             "email_address": req.body.email,
@@ -20,7 +21,7 @@ router.post('/mailchimp', function(req, res) {
             "LNAME": req.body.lName
         },
         headers: {
-            'Authorization': username + ' ' + password,
+            'Authorization': `${username} ${password}`,
             'Content-Type': 'application/json; charset=utf-8'
         }
     }
