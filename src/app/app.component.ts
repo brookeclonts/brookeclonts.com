3db1d8333b8707dd82eb336805a212ad932166b6
Brooke Clonts <brooke.clonts@ariix.com>
2017-01-25 13:02:36 -0700

    google analytics in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {AnimationService} from './services/animation.service';
+import { Location } from '@angular/common';
+import { AnimationService } from './services/animation.service';
+import{ Router, NavigationEnd } from '@angular/Router';
+declare var ga: Function;
 
 @Component({
     selector: 'app-root',
@@ -12,14 +15,29 @@ export class AppComponent implements OnInit {
     route;
     showLoaderIcon: boolean;
     move: boolean = false;
+    currentRoute: string;
 
-    constructor(private animate: AnimationService) {
+    constructor(private animate: AnimationService, private _router: Router, private location: Location) {
         animate.loaderAnnounced$.subscribe(
             (value: boolean) => {
             if (value) {
                 this.showLoader();
             } else {
                 this.loader = value;
+            }
+        });
+
+         _router.events.subscribe((event) => {
+            // Send GA tracking on NavigationEnd event. You may wish to add other
+            // logic here too or change which event to work with
+            if (event instanceof NavigationEnd) {
+                // When the route is '/', location.path actually returns ''.
+                let newRoute = this.location.path() || '/';
+                // If the route has changed, send the new route to analytics.
+                if (this.currentRoute !== newRoute) {
+                    ga('send', 'pageview', newRoute);
+                    this.currentRoute = newRoute;
+                }
             }
         });
     }
