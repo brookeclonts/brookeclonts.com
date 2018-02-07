78e45ed6efe3bb512291e70a46e5db7f4f76104e
Brooke Clonts <brookeclonts@workfront.com>
2018-02-06 17:57:55 -0700

    fixed server not serving api endpionts or static files

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ var external = require('./routes/api/external');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -32,16 +28,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+app.use(express.static('public'));
 
 app.use('/api/projects', projects);
 app.use('/api/books', books);
 app.use('/api/blogposts', blogposts);
 app.use('/api/users', users);
 app.use('/api/external', external);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
