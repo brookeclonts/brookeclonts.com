1891c56413c129e8acaa2e464ef8e1bee099fc61
Brooke Clonts <brookeclonts@workfront.com>
2019-09-07 01:04:59 -0600

    updated babel in package json

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,10 +1,6 @@
 require('./TwitterScripts');
 require('./FacebookScripts');
 
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
-environment.plugins.delete("UglifyJs");
-environment.plugins.set("UglifyJs", new UglifyJSPlugin());
-
 import React from 'react'
 import { hydrate } from 'react-dom'
 import App from './App'
